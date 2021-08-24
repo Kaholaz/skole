71a4592fa48c4ef0d6b4c239c8a2d253e38eead3
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-08-24 10:21:54 +0200

    gjort ferdig øvelse 2

---
+++ Dataing/1semester/Programering/2øving/oppgave1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Et år er skuddår dersom det er delelig med 4. Unntaket er hundreårene, de må
+ * være delelig med 400. Tegn aktivitetsdiagram som viser algoritmen for å finne
+ * ut om et år er skuddår. Årstallet skal leses inn fra brukeren. Sett opp
+ * testdata. Lag og prøv ut programmet.
+ */
+public class oppgave1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hvilket år vil du undersøke om er skuddår?");
+        int år = sc.nextInt();
+        sc.close();
+
+        boolean skuddår = false;
+        if (år % 4 == 0 && (år % 100 != 0 || år % 400 == 0)) {
+            skuddår = true;
+        }
+
+        System.out.println(år + " er" + ((skuddår) ? "" : " ikke") + " et skuddår.");
+    }
+}
---
+++ Dataing/1semester/Programering/2øving/oppgave2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * Lag et program som hjelper oss i forhold til følgende problemstilling:
+ * Kjøttdeig av merke A koster kr 35,90 for 450 gram, mens kjøttdeig av merke B
+ * koster kr 39,50 for 500 gram. Hvilket merke er billigst?
+ */
+public class oppgave2 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Kjøttdeig merkeA = new Kjøttdeig("merke A");
+        Kjøttdeig merkeB = new Kjøttdeig("merke B");
+
+        if (merkeA.kilopris > merkeB.kilopris) {
+            System.out.println(merkeB.navn + " er billigst.");
+        } else if (merkeA.kilopris < merkeB.kilopris) {
+            System.out.println(merkeA.navn + " er billigst.");
+        } else {
+            System.out.println("Begge merkene er like billig!");
+        }
+
+        sc.close();
+    }
+
+    /**
+     * Kjøttdeig er en klasse for å finne kiloprisen til kjøttdeig. Pris angis i
+     * kroner og vekt angis i gram.
+     */
+    static class Kjøttdeig {
+        double pris;
+        double vekt;
+        double kilopris;
+        String navn;
+
+        public Kjøttdeig(String _navn) {
+            navn = _navn;
+            System.out.println("Hvor mye koster " + navn);
+            pris = sc.nextDouble();
+            System.out.println("Hvor mange gram veier " + navn);
+            vekt = sc.nextDouble();
+
+            kilopris = (pris / vekt) * 1000;
+        }
+    }
+}
