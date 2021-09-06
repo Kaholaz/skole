9f09411e3dd5a850ff6b26f87daf4b3e433e6e33
Sebastian Bugge <sebastian@kaholaz.xyz>
2021-09-06 13:47:50 +0200

    Ferdig med øving6

--- Dataing/Programering1/6øving/Oppgave2.java
+++ Dataing/Programering1/6øving/Oppgave2.java
@@ -1,14 +1,121 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Oppgave2
  */
 public class Oppgave2 {
+    static Scanner sc = new Scanner(System.in);
+    static final String FEILMELDING = "Valget stemmer ikke overens med alternativene...\nVenligst prøv på nytt!";
 
     public static void main(String[] args) {
+        String tekst = "ååå";
+        Tekstanalyse t = new Tekstanalyse(tekst);
 
+        while (true) {
+            int valg = meny("Hva ønsker du å vite om teksten?",
+                    new String[] { "Antall forskjellige unike bokstaver", "Totalt antall bokstaver",
+                            "Hvor stor andel av teksten som ikke er bokstaver", "Antall forekomster av en gitt bokstav",
+                            "Hvilken/hvilke bokstaver som forekommer oftest" },
+                    "Avslutt");
+
+            if (valg == 6) {
+                break;
+            }
+
+            switch (valg) {
+                case 1:
+                    System.out.printf("Antall forskjellige bokstaver er %d.", t.finnAntallUnike());
+                    break;
+                case 2:
+                    System.out.printf("Antall bokstaver er %d.", t.finnAntallBokstaver());
+                    break;
+                case 3:
+                    System.out.printf("Andelen av teksten som ikke er bokstaver er %.2f%%.",
+                            t.finnAndelIkkeBokstaver() * 100);
+                    break;
+                case 4:
+                    System.out.println("Hvilken bokstav ønsker du å undersøke?");
+                    String in = sc.next();
+                    char bokstav = in.charAt(0);
+                    System.out.printf("Andelen av teksten som er bokstaven '%s' er %d.", bokstav,
+                            t.finnForekomster(bokstav));
+                    break;
+                case 5:
+                    String[] chars = t.finnVanligsteBokstav();
+                    System.out.printf("%s er [%s]",
+                            ((chars.length == 1) ? "Den vanligste bokstaven" : "De vanligste bokstavene"),
+                            String.join(", ", chars));
+                    break;
+                default:
+                    throw new IllegalStateException("valg har en uventet verdi");
+            }
+            sc.next();
+        }
+    }
+
+    /**
+     * Denne funksjonen viser en valgmeny for brukeren og validerer brukerinputet.
+     * 
+     * @param spørsmål Spørsmålet som printes øverst i valgmenyen
+     * @param valg     Dette er svaralternativene
+     * @return Retuernerer et heltall som korresponderer med svaralternativene der 1
+     *         er det første alternativet.
+     */
+    static int meny(String spørsmål, String[] valg) {
+        int tall;
+        boolean fail = false;
+        while (true) {
+            clearScreen();
+            System.out.println(spørsmål);
+            for (int i = 0; i < valg.length; i++) {
+                System.out.println((i + 1) + ". " + valg[i]);
+            }
+            if (fail) {
+                System.out.println(FEILMELDING);
+            }
+
+            if (sc.hasNextInt()) { // Hvis brukeren ikke taster inn et heltall, blir tallet satt til -1 slik at
+                                   // brukeren blir vist feilmeldingen nedenfor.
+                tall = sc.nextInt();
+            } else {
+                sc.next();
+                tall = -1;
+            }
+
+            if (tall < 1 || tall > valg.length) {
+                fail = true;
+                continue;
+            }
+            break;
+        }
+        return tall;
+    }
+
+    /**
+     * Denne funksjonen viser en valgmeny for brukeren og validerer brukerinputet.
+     * 
+     * @param spørsmål  Spørsmålet som printes øverst i valgmenyen
+     * @param valg      Dette er svaralternativene
+     * @param sisteValg Åpner muligheten for å legge til et siste svaralternativ,
+     *                  gjerne 'Avslutt' eller 'Tilbake'
+     * @return Retuernerer et heltall som korresponderer med svaralternativene der 1
+     *         er det første alternativet.
+     */
+    static int meny(String spørsmål, String[] valg, String sisteValg) {
+        String[] _valg = new String[valg.length + 1];
+        for (int i = 0; i < valg.length; i++) {
+            _valg[i] = valg[i];
+        }
+        _valg[valg.length] = sisteValg;
+        return meny(spørsmål, _valg);
+    }
+
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
 
@@ -72,7 +179,7 @@ class Tekstanalyse {
         }
     }
 
-    public char[] finnVanligsteBokstav() {
+    public String[] finnVanligsteBokstav() {
         int max = Arrays.stream(getAntallBokstaver()).reduce(0, (a, b) -> Math.max(a, b));
         List<Character> bokstaver = new ArrayList<Character>();
         for (int i = 0; i < getAntallBokstaver().length; i++) {
@@ -80,14 +187,14 @@ class Tekstanalyse {
                 bokstaver.add(finnBokstav(i));
             }
         }
-        char[] result = new char[bokstaver.size()];
+        String[] result = new String[bokstaver.size()];
         for (int i = 0; i < result.length; i++) {
-            result[i] = bokstaver.get(i);
+            result[i] = String.valueOf(bokstaver.get(i));
         }
         return result;
     }
 
-    public void nullstill() {
+    private void nullstill() {
         for (int i = 0; i < getAntallTegn().length; i++) {
             antallTegn[i] = 0;
         }
---
+++ Dataing/Programering1/6øving/Oppgave3.java
@@ -0,0 +1,124 @@
+public class Oppgave3 {
+
+    public static void main(String[] args) {
+        Matrise a = new Matrise(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } });
+        Matrise b = new Matrise(
+                new int[][] { new int[] { 10, 11, 12 }, new int[] { 13, 14, 15 }, new int[] { 16, 17, 18 } });
+        Matrise c = new Matrise(new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 5, 6 } });
+        System.out.println(a.add(b));
+        System.out.println(a.multi(b));
+        System.out.println(a.multi(c));
+        System.out.println(a.add(c));
+    }
+}
+
+class Matrise {
+    int n;
+    int m;
+    private int[][] matrise;
+
+    Matrise(Matrise matrise) {
+        this.n = matrise.n;
+        this.m = matrise.m;
+
+        int[][] array2d = new int[getN()][getM()];
+
+        for (int n = 0; n < getN(); n++) {
+            for (int m = 0; m < getM(); m++) {
+                array2d[n][m] = matrise.get(n, m);
+            }
+        }
+
+        this.matrise = array2d.clone();
+    }
+
+    Matrise(int[][] array2d) {
+        int ySize = array2d.length;
+        int xSize = array2d[0].length;
+        for (int[] row : array2d) {
+            if (row.length != xSize) {
+                throw new IllegalArgumentException("Alle radene må ha samme lengde...");
+            }
+        }
+
+        this.matrise = array2d.clone();
+        setN(ySize);
+        setM(xSize);
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int[] getDimensjon() {
+        return new int[] { getN(), getM() };
+    }
+
+    public int get(int n, int m) {
+        return matrise[n][m];
+    }
+
+    public void set(int n, int m, int verdi) {
+        matrise[n][m] = verdi;
+    }
+
+    public String toString() {
+        String result = "[[";
+        for (int n = 0; n < getN(); n++) {
+            if (n != 0) {
+                result = result.concat("\n");
+            }
+            for (int m = 0; m < getM(); m++) {
+                result = result.concat(this.get(n, m) + ", ");
+            }
+            result = result.concat("]");
+        }
+        result = result.concat("]");
+        return result;
+    }
+
+    Matrise add(Matrise matrise) {
+        if (!(this.getN() == matrise.getN() && this.getM() == matrise.getM())) {
+            throw new IllegalArgumentException("Dimensjonene til de to matrisene må være like");
+        }
+        int[][] array2d = new int[getN()][getM()];
+
+        for (int n = 0; n < getN(); n++) {
+            for (int m = 0; m < getM(); m++) {
+                array2d[n][m] = this.get(n, m) + matrise.get(n, m);
+            }
+        }
+
+        return new Matrise(array2d);
+    }
+
+    Matrise multi(Matrise matrise) {
+        if (this.getM() != matrise.getN()) {
+            throw new IllegalArgumentException("Dimensjonene stemmer ikke overens");
+        }
+
+        int p = this.getM() - 1;
+        int[][] array2d = new int[this.getN()][matrise.getM()];
+        for (int n = 0; n < this.getN(); n++) {
+            for (int m = 0; m < matrise.getM(); m++) {
+                array2d[n][m] = 0;
+                for (int i = 0; i < p; i++) {
+                    array2d[n][m] += this.get(n, p) * matrise.get(p, m);
+                }
+            }
+        }
+        return new Matrise(array2d);
+    }
+}
